59d81f0b4707905717901aea01d401953e72b838
rahma <fijriatirahmaturrizqi@gmail.com>
2024-11-28 08:20:48 +0700

    update tampilan matriks

--- kuis2.java
+++ kuis2.java
@@ -32,6 +32,49 @@ public class kuis2 {
             }
         }
 
+        System.out.println("=====================");
+        for (int i = 0; i < matriks1.length; i++) {
+            for (int j = 0; j < matriks1[0].length; j++) {
+                if (j == 0) {
+                    System.out.print("| " + matriks1[i][j] + " ");
+                } else if (j == matriks1[0].length - 1) {
+                    System.out.print(matriks1[i][j] + " |");
+                } else {
+                    System.out.print(matriks1[i][j] + " ");
+                }
+            }
+            if (i == 0) {
+                System.out.print(" + ");
+            } else {
+                System.out.print("   ");
+            }
+            for (int j = 0; j < matriks2[0].length; j++) {
+                if (j == 0) {
+                    System.out.print("| " + matriks2[i][j] + " ");
+                } else if (j == matriks2[0].length - 1) {
+                    System.out.print(matriks2[i][j] + " |");
+                } else {
+                    System.out.print(matriks2[i][j] + " ");
+                }
+            }
+            if (i == 0) {
+                System.out.print(" = ");
+            } else {
+                System.out.print("   ");
+            }
+            for (int j = 0; j < matriksHasilPenjumlahan[0].length; j++) {
+                if (j == 0) {
+                    System.out.print("| " + matriksHasilPenjumlahan[i][j] + " ");
+                } else if (j == matriksHasilPenjumlahan[0].length - 1) {
+                    System.out.print(matriksHasilPenjumlahan[i][j] + " |");
+                } else {
+                    System.out.print(matriksHasilPenjumlahan[i][j] + " ");
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.println("=====================");
+
         System.out.println("Hasil penjumlahan matriks:");
         for (int i = 0; i < matriksHasilPenjumlahan.length; i++) {
             for (int j = 0; j < matriksHasilPenjumlahan[0].length; j++) {
